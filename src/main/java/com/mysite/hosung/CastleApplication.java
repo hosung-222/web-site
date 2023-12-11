e4b286d33c3d03fcd88ba7a923fa41ef60ef43ee
hosung-222 <gik11kr@naver.com>
2023-12-11 14:15:33 +0900

    first commit

---
+++ src/main/java/com/mysite/hosung/CastleApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.hosung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/hosung/CastleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.hosung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
