df0259895494c5af79baff95d0cc1054f2b47f94
hosung-222 <gik11kr@naver.com>
2023-12-11 15:18:22 +0900

    :sparkles: [feat] : 루트 디렉토리 설정

---
+++ src/main/java/com/mysite/hosung/web/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.mysite.hosung.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
+    }
+}
