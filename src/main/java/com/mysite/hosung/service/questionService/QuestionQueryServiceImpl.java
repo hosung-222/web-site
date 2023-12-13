7bf9986c482d74960135bb5b0a44bcb5f477955b
hosung-222 <gik11kr@naver.com>
2023-12-13 13:38:19 +0900

    :recycle: [chore] : 질문 목록 최신순 정렬 변경

--- src/main/java/com/mysite/hosung/service/questionService/QuestionQueryServiceImpl.java
+++ src/main/java/com/mysite/hosung/service/questionService/QuestionQueryServiceImpl.java
@@ -3,12 +3,14 @@ package com.mysite.hosung.service.questionService;
 import com.mysite.hosung.apiPayload.DataNotFoundException;
 import com.mysite.hosung.domain.Question;
 import com.mysite.hosung.repository.QuestionRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +22,9 @@ public class QuestionQueryServiceImpl implements QuestionQueryService{
 
     @Override
     public Page<Question> getQuestionList(int page) {
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createdAt"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return questionRepository.findAll(pageable);
     }
 
